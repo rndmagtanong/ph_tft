c1aae79a380a48091313afa4ce28dce344454bb4
rndmagtanong <rndmagtanong@gmail.com>
2023-03-16 19:27:46 +0800

    creating pipeline

--- ph_data.py
+++ ph_data.py
@@ -134,65 +134,132 @@ def get_match_data(match_ids):
 
     return holder
 
-def percent_missing_data(match_data):
-    # get number of missing data points per column
-    missing_values_count = match_data.isnull().sum()
+### data pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
 
-    # how many missing values do we have?
-    total_cells = np.product(match_data.shape)
-    total_missing = missing_values_count.sum()
+class DoubleUpDropper(BaseEstimator, TransformerMixin):
 
-    # percent of missing data
-    percent_missing = (total_missing / total_cells) * 100
-    print('Percent Missing of Data: ' + str(percent_missing))
+    def fit(self, X, y = None):
+        return self
+    
+    def transform(self, X):
+        return X[X['partner_group_id'].isnull()]
+    
+class NaNDropper(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, y = None):
+        return self
+    
+    def transform(self, X):
+        return X.dropna(how = 'all').dropna(axis = 'columns', how = 'all')
+    
+class CorruptedDropper(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, y = None):
+        return self
+    
+    def transform(self, X):
+        corrupted_features = ['units_5_items_0', 'units_5_items_1',	
+                            'units_5_items_2', 'units_6_items_0',
+                            'units_6_items_1', 'units_6_items_2',
+                            'units_7_items_0', 'units_7_items_1',	
+                            'units_7_items_2', 'units_3_items_0',
+                            'units_3_items_1', 'units_0_items_0',
+                            'units_1_items_0', 'units_1_items_1',	
+                            'units_2_items_0', 'units_2_items_1',	
+                            'units_2_items_2', 'units_1_items_2',
+                            'units_4_items_0', 'units_4_items_1',	
+                            'units_4_items_2', 'units_0_items_1',	
+                            'units_3_items_2', 'units_0_items_2',	
+                            'units_8_items_0', 'units_8_items_1',	
+                            'units_8_items_2']
+        for feature in corrupted_features:
+            try:
+                X = X.drop(feature, axis = 'columns')
+            except:
+                continue
+
+            return X
+        
+class ResetIndex(BaseEstimator, TransformerMixin):
+    def fit(self, X, y = None):
+        return self
+    
+    def transform(self, X):
+        return X.reset_index(drop = True)
+
+class DescribeMissing(BaseEstimator, TransformerMixin):
+    
+    def fit(self, X, y = None):
+        return self
+    
+    def transform(self, X):
+        # get number of missing data points per column
+        missing_values_count = X.isnull().sum()
+
+        # how many missing values do we have?
+        total_cells = np.product(X.shape)
+        total_missing = missing_values_count.sum()
+
+        # percent of missing data
+        percent_missing = (total_missing / total_cells) * 100
+        print('Percent Missing of Data: ' + str(percent_missing))
+
+        return X
+    
+from sklearn.pipeline import Pipeline
+
+pipe_unprocessed = Pipeline([
+       ("double_up_dropper", DoubleUpDropper()),
+       ("nandrop", NaNDropper()),
+       ("corruptdropper", CorruptedDropper()),
+       ("resetindex", ResetIndex()),
+       ("nanpercent", DescribeMissing())
+])
+
+class TrainDropper(BaseEstimator, TransformerMixin):
+
+    def fit(self, X, y = None):
+        return self
+    
+    def transform(self, X):
+        # remove features that don't help with training the data
+        non_training_features = ['companion_content_ID', 'companion_item_ID',
+                                'companion_skin_ID', 'companion_species',
+                                'gold_left', 'players_eliminated']
+        
+        for feature in non_training_features:
+            try:
+                X = X.drop(feature, axis = 'columns')
+            except:
+                continue
+        
+        return X
+    
+class OutlierRemover(BaseEstimator, TransformerMixin):
+    def fit(self, X, y = None):
+        return self
+    
+    def transform(self, X):
+        # remove outliers (10% threshold to not remove level 8 data)
+        threshold = int(len(X) * 0.1)
+        X = X.dropna(axis = 1, thresh = threshold)
 
 def simple_data_pipeline(match_data, filename):
-    # drop columns from double up
-    match_data = match_data[match_data['partner_group_id'].isnull()]
-
-    # drop all empty rows and columns
-    match_data = match_data.dropna(how = 'all').dropna(axis = 'columns', how = 'all')
-
-    # remove corrupted features
-    corrupted_features = ['units_5_items_0', 'units_5_items_1',	
-                          'units_5_items_2', 'units_6_items_0',
-                          'units_6_items_1', 'units_6_items_2',
-                          'units_7_items_0', 'units_7_items_1',	
-                          'units_7_items_2', 'units_3_items_0',
-                          'units_3_items_1', 'units_0_items_0',
-                          'units_1_items_0', 'units_1_items_1',	
-                          'units_2_items_0', 'units_2_items_1',	
-                          'units_2_items_2', 'units_1_items_2',
-                          'units_4_items_0', 'units_4_items_1',	
-                          'units_4_items_2', 'units_0_items_1',	
-                          'units_3_items_2', 'units_0_items_2',	
-                          'units_8_items_0', 'units_8_items_1',	
-                          'units_8_items_2']
-    for feature in corrupted_features:
-        try:
-            match_data = match_data.drop(feature, axis = 'columns')
-        except:
-            continue
-
-    # reindex dataframe
-    match_data.reset_index(drop = True)
-
-    # describe missing data
-    percent_missing_data(match_data)
 
     # write csv for data analysis
     match_data.to_csv('unprocessed_' + filename + '.csv', index = False)
 
-    # remove features that don't help with training the data
-    non_training_features = ['companion_content_ID', 'companion_item_ID',
-                             'companion_skin_ID', 'companion_species',
-                             'gold_left', 'players_eliminated']
+    # # remove features that don't help with training the data
+    # non_training_features = ['companion_content_ID', 'companion_item_ID',
+    #                          'companion_skin_ID', 'companion_species',
+    #                          'gold_left', 'players_eliminated']
     
-    for feature in non_training_features:
-        try:
-            match_data = match_data.drop(feature, axis = 'columns')
-        except:
-            continue
+    # for feature in non_training_features:
+    #     try:
+    #         match_data = match_data.drop(feature, axis = 'columns')
+    #     except:
+    #         continue
 
     # remove outliers (10% threshold to not remove level 8 data)
     threshold = int(len(match_data) * 0.1)
